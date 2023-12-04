e7d441944bc047ba07a7eb35168aa26a39d62954
Dikshit-maan <149257007+Dikshit-maan@users.noreply.github.com>
2023-12-04 22:09:17 +0000

    Conti..

--- Prog.web/script.js
+++ Prog.web/script.js
@@ -1,8 +1,12 @@
 
 var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
 document.addEventListener("mousemove",function(dets){
-    crsr.Style.left = dets.x+"px"
-    crsr.Style.top = dets.y+"px"
+    crsr.Style.left = dets.x + "px"
+    crsr.Style.top = dets.y + "px"
+    blur.Style.left = dets.x - 150 + "px"
+    blur.Style.top = dets.y - 150 + "px"
 })
 
 
