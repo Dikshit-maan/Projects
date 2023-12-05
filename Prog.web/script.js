c41ec171dcde08ddf64f3d843b14841d0f73030c
Dikshit-maan <149257007+Dikshit-maan@users.noreply.github.com>
2023-12-05 06:56:46 +0000

    Cursor effect not working

--- Prog.web/script.js
+++ Prog.web/script.js
@@ -2,15 +2,19 @@
 var crsr = document.querySelector("#cursor")
 var blur = document.querySelector("#cursor-blur")
 
+// document.addEventListener("mousemove",function(dets){
+//     crsr.Style.left = dets.x + "px"
+//     crsr.Style.top = dets.y + "px"
+//     blur.Style.left = dets.x - 150 + "px"
+//     blur.Style.top = dets.y - 150 + "px"
+// })
+
+
 document.addEventListener("mousemove",function(dets){
-    crsr.Style.left = dets.x + "px"
-    crsr.Style.top = dets.y + "px"
-    blur.Style.left = dets.x - 150 + "px"
-    blur.Style.top = dets.y - 150 + "px"
+    
+    crsr.Styl.left = dets.x + "px"
+    crsr.Styl.top = dets.y + "px"
 })
-
-
-
 
 
 
