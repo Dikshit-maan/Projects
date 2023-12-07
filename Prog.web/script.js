cd4524381c716646ba73db0fe042b7c1716ea5ab
Dikshit-maan <149257007+Dikshit-maan@users.noreply.github.com>
2023-12-07 13:54:06 +0000

    Adjecting the cursor

--- Prog.web/script.js
+++ Prog.web/script.js
@@ -3,8 +3,8 @@ var crsr = document.querySelector("#cursor")
 var blur = document.querySelector("#cursor-blur")
 
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x + "px"
-    crsr.style.top = dets.y + "px"
+    crsr.style.left = dets.x - 10 + "px"
+    crsr.style.top = dets.y - 10 + "px"
     blur.style.left = dets.x - 150 + "px"
     blur.style.top = dets.y - 150 + "px"
 })
