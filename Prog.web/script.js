de61b97b55bf12b2ccf796dcba9ac81253de75e4
Dikshit-maan <149257007+Dikshit-maan@users.noreply.github.com>
2023-12-07 10:38:59 +0000

    commit

--- Prog.web/script.js
+++ Prog.web/script.js
@@ -2,19 +2,19 @@
 var crsr = document.querySelector("#cursor")
 var blur = document.querySelector("#cursor-blur")
 
-// document.addEventListener("mousemove",function(dets){
-//     crsr.Style.left = dets.x + "px"
-//     crsr.Style.top = dets.y + "px"
-//     blur.Style.left = dets.x - 150 + "px"
-//     blur.Style.top = dets.y - 150 + "px"
-// })
-
-
 document.addEventListener("mousemove",function(dets){
-    
-    crsr.Styl.left = dets.x + "px"
-    crsr.Styl.top = dets.y + "px"
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
+    blur.style.left = dets.x - 150 + "px"
+    blur.style.top = dets.y - 150 + "px"
 })
+
+
+// document.addEventListener("mousemove",function(dets){
+    
+//     crsr.Styl.left = dets.x + "px"
+//     crsr.Styl.top = dets.y + "px"
+// })
 
 
 
